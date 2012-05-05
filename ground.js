cf696e5274050072bb3f7247037a4f37314fbcb3
Rico P <MailToRico@googlemail.com>
2012-05-05 14:24:32 +0200

    Hallo zu Ballo

--- ground.js
+++ ground.js
@@ -58,7 +58,7 @@ module.createGround = function (gl, projection) {
     var texture = gl.createTexture(); 
     var image = new Image(); 
     image.onload = function() {
-		console.log("Hallo"); 
+		console.log("Ballo"); 
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
