d910b14aa791e63ab5136cdbb6191261f0d469c2
Rico P <MailToRico@googlemail.com>
2012-06-05 11:37:38 +0200

    shader module

---
+++ src/glt.shader.js
@@ -0,0 +1,33 @@
+(function(GLT) {
+"use strict"; 
+
+function compileProgram(gl, programsource) {
+	var defines = ["#define VERTEX\n", "#define FRAGMENT\n"]; 
+	var shader = [gl.createShader(gl.VERTEX_SHADER), gl.createShader(gl.FRAGMENT_SHADER)]; 
+	var program = gl.createProgram(); 
+	var s = null; 
+	var info = ""; 
+
+	for(var i = 0; i != defines.length; i++) {
+		s = shader[i]; 
+		gl.shaderSource(s, defines[i] + programsource); 
+		gl.compileShader(s); 		
+		
+		if( info = gl.getShaderInfoLog(s) ) {
+			throw new Error(info); 
+		}
+
+		gl.attachShader(program, s); 
+	}
+
+	gl.linkProgram(program); 
+	if( info = gl.getProgramInfoLog(program) ) {
+		throw new Error(info); 
+	} 
+	
+	return program; 
+}
+
+GLT.SHADER = {}; 
+GLT.SHADER.compileProgram = compileProgram;  
+}(GLT)); 
